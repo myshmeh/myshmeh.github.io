4d33fef953988678756f56bd83dc7ebfd50d77c2
myshmeh <includek06enhavn@gmail.com>
2016-06-22 06:40:08 +0200

    Add files via upload

--- js/Particle/main.js
+++ js/Particle/main.js
@@ -1,6 +1,12 @@
 var canvas;
 var t;
+//draw
 var p, p2;
+//draw2
+var x, y, r;
+var noisedR;
+var xoff, yoff;
+var val;
 function setup() {
   //init canvas
   var  h;
@@ -13,20 +19,29 @@ function setup() {
   
   //init var
   t = 0;
+  //draw
   p = new Vec2();
   p2 = new Vec2();
+  //draw2
+  x = y = 0;
+  r = height * 0.6;
+  noisedR = 0;
+  xoff = yoff = 0;
+  val = 1;
 }
 
 function draw() {
   Update();
   background(181, 39, 53);
-  Draw();
+  //Draw();
+  Draw2();
 }
 
 function Update(){
 	t++;
 }
 
+//*--Draw
 function Draw(){
 	translate(width/2, height/2);
 	//stroke(255);
@@ -45,4 +60,25 @@ function Draw(){
 		line(p.x, p.y, p2.x, p2.y+20);
 	}
 	resetMatrix();
+}
+
+//*--Draw 2
+function Draw2(){
+	translate(width*0.495, height/2);
+	strokeWeight(2);
+	stroke(255);
+	var i;
+	for(i=0; i<360; i++){
+		xoff = sin(radians(i)) * sin(radians(i*val)) * 0.85;
+		noisedR = noise(xoff, yoff) * r;
+		x = cos(radians(i)) * noisedR; //cos(angle) == x / r -> x == cos(angle) * r
+		y = sin(radians(i)) * noisedR; //sin(angle) == y / r -> y == sin(angle) * r
+		line(0, 0, x, y); //draw line from center to further based on angle(i)
+	}
+	yoff += 0.01;
+	resetMatrix();
+}
+
+function mouseClicked() {
+	val = (val+1) % 7 + 1;
 }
--- js/Particle/particle.js
+++ js/Particle/particle.js
@@ -2,33 +2,3 @@ function Vec2(r){
 	this.x = 0;
 	this.y = 0;
 }
-
-function Particle(x, y, hspd, vspd){
-	this.x = x;
-	this.y = y;
-	this.hspd = hspd;
-	this.vspd = vspd;
-	
-	this.Update = function(){
-		if(this.isOutOfScreen()){
-			this.x = mouseX;
-			this.y = mouseY;
-		}
-		//apply gravity
-		this.vspd += 1;
-		this.y += this.vspd;
-		this.x += this.hspd;
-	}
-	
-	this.Draw = function(){
-		stroke(255);
-		strokeWeight(3);
-		point(this.x, this.y);
-	}
-	
-	this.isOutOfScreen = function(){
-		if(this.y > height)
-			return true;
-		return false;
-	}
-}
