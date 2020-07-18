6bd6aa8414dd78aa52d3662c4fb095e055b5dee6
myshmeh <myshmeh@gmail.com>
2020-07-18 20:58:49 +0900

    commented out plugin loads in preloader in gokitmp

--- gokitmp/js/scenes/preloader.js
+++ gokitmp/js/scenes/preloader.js
@@ -4,28 +4,28 @@ class Preloader extends Phaser.Scene {
         super({
             key: 'preloader',
             // reference: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/webfontloader/
-            pack: {
-                files: [{
-                    type: 'plugin',
-                    key: 'rexwebfontloaderplugin',
-                    url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexwebfontloaderplugin.min.js',
-                    start: true
-                }]
-            }
+            // pack: {
+            //     files: [{
+            //         type: 'plugin',
+            //         key: 'rexwebfontloaderplugin',
+            //         url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexwebfontloaderplugin.min.js',
+            //         start: true
+            //     }]
+            // }
         });
     }
 
     preload() {
-        this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true);
-        this.plugins.get('rexwebfontloaderplugin').addToScene(this);
-        const fontLoadConfig = {
-            google: {
-                // for developing speed, avoid fetching press start 2p
-                // families: [FONTS.MONOTON, FONTS.CREEPSTER, FONTS.PRESS_START_2P]
-                families: [FONTS.MONOTON, FONTS.CREEPSTER]
-            }
-        }
-        this.load.rexWebFont(fontLoadConfig);
+        // this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true);
+        // this.plugins.get('rexwebfontloaderplugin').addToScene(this);
+        // const fontLoadConfig = {
+        //     google: {
+        //         // for developing speed, avoid fetching press start 2p
+        //         // families: [FONTS.MONOTON, FONTS.CREEPSTER, FONTS.PRESS_START_2P]
+        //         families: [FONTS.MONOTON, FONTS.CREEPSTER]
+        //     }
+        // }
+        // this.load.rexWebFont(fontLoadConfig);
         this.load.atlas('sprites', 'img/spritearray.png', 'img/spritearray.json');
         this.load.image('asphalt', 'img/asphalt.png');
         this.load.image('key', 'img/key.png');
